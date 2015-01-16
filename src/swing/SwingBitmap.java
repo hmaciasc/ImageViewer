5a921d9d24de271827f7af26160c81c7611b50ec
Hector <hectormaciasc@gmail.com>
2015-01-16 21:59:01 +0000

    Image load implemented using lazy load.

---
+++ src/swing/SwingBitmap.java
@@ -0,0 +1,16 @@
+package swing;
+
+import java.awt.image.BufferedImage;
+import model.Bitmap;
+
+public class SwingBitmap implements Bitmap{
+    private BufferedImage bufferedImage;
+
+    public SwingBitmap(BufferedImage bufferedImage) {
+        this.bufferedImage = bufferedImage;
+    }
+    
+    public BufferedImage getBufferedImage(){
+        return bufferedImage;
+    }
+}
